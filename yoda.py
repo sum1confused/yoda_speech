9a3823f3fa5646ead923d80eb88e1c0cadbe9477
sum1confused <64354473+sum1confused@users.noreply.github.com>
2021-01-11 11:17:54 +0000

    Add files via upload

--- yoda.py
+++ yoda.py
@@ -1,16 +1,27 @@
+#Takes user input
 speech = str(input())
  
+ #splits the string into a list
 speech = (speech[0:].split())
 
+#puts the first 2 words at the end of the list
 
-speech.append (speech[0])
-speech.append (speech[1])
-speech.remove (speech[0])
-speech.remove (speech[0])
+if len(speech) < 3:
+    print  (" ".join(speech))
+else:
+
+    speech.append (speech[0])
+    speech.append (speech[1])
+    speech.remove (speech[0])
+    speech.remove (speech[0])
+ # rejoins the list into a string with spaces
+    speech =  (" ".join(speech))
+
+    print(speech)
 
 
 
 
-speech =  (" ".join(speech))
 
-print(speech)
+
+# I did this for fun and practice in a few minutes, I plan on making it a full fledged program that actually makes more sense.
