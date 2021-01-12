23626805001d0ea53c3e4e8290adfd3b3073a6bb
sum1confused <64354473+sum1confused@users.noreply.github.com>
2021-01-12 08:04:28 +0000

    Add files via upload

--- yoda.py
+++ yoda.py
@@ -1,23 +1,34 @@
 #Takes user input
 speech = str(input())
+
+f = False
  
  #splits the string into a list
 speech = (speech[0:].split())
+for x in speech:
+	if x == "?":
+		f = True
+		speech.remove("?")
 
+		
 #puts the first 2 words at the end of the list
 
 if len(speech) < 3:
     print  (" ".join(speech))
 else:
-
-    speech.append (speech[0])
-    speech.append (speech[1])
-    speech.remove (speech[0])
-    speech.remove (speech[0])
+			
+	speech.append (speech[0])
+	speech.append (speech[1])
+	speech.remove (speech[0])
+	speech.remove (speech[0])
+	
+if f == True:
+	speech.append("?")    
  # rejoins the list into a string with spaces
-    speech =  (" ".join(speech))
+speech = (" ".join(speech))
 
-    print(speech)
+print (speech)
+
 
 
 
